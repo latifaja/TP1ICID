3b9fec205b0a35b05e56e3d3b4f0c8ce3f62dabb
latifaja <#2662019Ma?>
2025-02-24 17:03:53 +0100

    injection via setter

--- src/main/java/pres/PresentationV1.java
+++ src/main/java/pres/PresentationV1.java
@@ -1,7 +1,16 @@
 package pres;
 
+import dao.DaoImpl;
+import dao.IDao;
+import metier.IMetier;
+import metier.IMetierImpl;
+
 public class PresentationV1 {
     public static void main(String[] args) {
+        IMetierImpl metier = new IMetierImpl();
+        DaoImpl d = new DaoImpl();
+        metier.setDao(d);
+        System.out.println(metier.calcul());
 
     }
 }
