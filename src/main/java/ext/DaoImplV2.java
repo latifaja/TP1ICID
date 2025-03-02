03072d181a6c277fed13905ec5e438b99ce651af
latifaja <#2662019Ma?>
2025-03-03 00:02:30 +0100

    injection via setter

--- src/main/java/ext/DaoImplV2.java
+++ src/main/java/ext/DaoImplV2.java
@@ -6,7 +6,7 @@ public class DaoImplV2 implements IDao {
     @Override
     public double getData() {
         System.out.println("version web service ");
-        double t=11;
+        double t=77;
         return t;
     }
 }
--- src/main/java/pres/PresAvecSpring.java
+++ src/main/java/pres/PresAvecSpring.java
@@ -1,4 +1,18 @@
 package pres;
 
+import metier.IMetier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class PresAvecSpring {
+    public static void main(String[] args) {
+        // Correct class name: ClassPathXmlApplicationContext
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+
+        // You can now retrieve beans from the context (example)
+        // MyBean myBean = context.getBean(MyBean.class);
+        IMetier metier = (IMetier) context.getBean("metier");
+        System.out.println("res" + metier.calcul());
+    }
+
 }
